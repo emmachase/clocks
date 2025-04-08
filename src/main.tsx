37816873df75d8b3ca684e6fe0b8e1a977bf2e15
Emma <me@its-em.ma>
2025-04-08 16:13:50 -0700

    feat: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { scan } from "react-scan"; // must be imported before React and React DOM
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
-scan({enabled: true})
+scan({ enabled: true });
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Analytics />
+  </StrictMode>
+);
